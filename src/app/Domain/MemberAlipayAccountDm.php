c54366f9743fec4955c81d033567d2b79cce37b6
kukujiabo <kukujiabo@163.com>
2018-06-27 15:51:05 +0800

    123

--- src/app/Domain/MemberAlipayAccountDm.php
+++ src/app/Domain/MemberAlipayAccountDm.php
@@ -1,7 +1,21 @@
 <?php
 namespace App\Domain;
 
+use App\Library\RedisClient;
+
 class MemberAlipayAccountDm {
+
+  protected $_member; 
+
+  public function __construct() {
+  
+    $requestHeader = getallheaders();
+  
+    $auth = RedisClient::get('member_auth', $requestHeader['CX-TOKEN']);
+
+    $this->_member = $auth;
+
+  }
 
   public function create($data) {
   
