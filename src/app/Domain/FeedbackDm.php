8ae29e37a768f9ba9c1a3a23b2a5aa3f06eecebb
kukujiabo <kukujiabo@163.com>
2018-07-11 12:53:59 +0800

    123

--- src/app/Domain/FeedbackDm.php
+++ src/app/Domain/FeedbackDm.php
@@ -3,13 +3,41 @@ namespace App\Domain;
 
 class FeedbackDm {
 
+  protected $_member; 
+
+  public function __construct() {
+  
+    $requestHeader = getallheaders();
+  
+    $auth = RedisClient::get('member_auth', $requestHeader['CX-TOKEN']);
+
+    $this->_member = $auth;
+
+  }
+
   public function addFeedback($params) {
+
+    if (!$this->_member) {
+    
+      return null;
+    
+    }
+
+    $params['member_id'] = $this->_member->id
   
     return \App\request('App.Feedback.AddFeedback', $params);
   
   }
 
   public function getList($params) {
+
+    if (!$this->_member) {
+    
+      return null;
+    
+    }
+
+    $params['member_id'] = $this->_member->id
   
     return \App\request('App.Feedback.GetList', $params);
   
